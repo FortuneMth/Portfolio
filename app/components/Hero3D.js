a3a46ef6191f78196de77b92ba53582c77a5ab8b
Fortune Mthembu <fortunemthembu3@gmail.com>
2025-04-22 03:29:49 +0200

    Fix image paths for Man2,jivejunction

--- app/components/Hero3D.js
+++ app/components/Hero3D.js
@@ -45,7 +45,7 @@ const Hero3D = () => {
               className="col-span-3 md:col-span-1 h-[400px] md:h-[400px] relative"
             >
               <Image
-                src="Man2.png" // Replace with your actual image path
+                src="/Man2.png" // Replace with your actual image path
                 alt="Design concept visualization"
                 fill
                 className="object-cover object-center"
